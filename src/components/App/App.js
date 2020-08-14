58a1de6022376207e7be4cd7ecc1accc440bc322
benz0is <rokinijas@gmail.com>
2020-08-14 16:19:39 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+function App() {
+  return (
+    <div>
+      <h1>Convert</h1>;
+      <CurrencyRow />
+    </div>
+  );
+}
+export default App;
